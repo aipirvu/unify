5a8934fac4811a019e3c2448188ef72b58e0e708
Alexandru Pirvu <aipirvu@gmail.com>
2017-02-22 19:17:41 +0200

    Code cleanup

--- Unify.Android/app/src/main/java/com/owlcreativestudio/unify/Helpers/CameraPreview.java
+++ Unify.Android/app/src/main/java/com/owlcreativestudio/unify/Helpers/CameraPreview.java
@@ -12,11 +12,6 @@ import java.util.List;
 public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
     private SurfaceHolder mHolder;
     private Camera mCamera;
-
-    public void setCamera(Camera mCamera) {
-        this.mCamera = mCamera;
-    }
-
     private String TAG = "CameraPreview";
     private List<Camera.Size> mSupportedPreviewSizes;
     private Camera.Size mPreviewSize;
@@ -27,14 +22,15 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
         mSupportedPreviewSizes = mCamera.getParameters().getSupportedPreviewSizes();
 
-        // Install a SurfaceHolder.Callback so we get notified when the
-        // underlying surface is created and destroyed.
         mHolder = getHolder();
         mHolder.addCallback(this);
     }
 
+    public void setCamera(Camera mCamera) {
+        this.mCamera = mCamera;
+    }
+
     public void surfaceCreated(SurfaceHolder holder) {
-        // The Surface has been created, now tell the camera where to draw the preview.
         try {
             mCamera.setPreviewDisplay(holder);
             mCamera.startPreview();
@@ -44,27 +40,19 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     }
 
     public void surfaceDestroyed(SurfaceHolder holder) {
-        // empty. Take care of releasing the Camera preview in your activity.
     }
 
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-        // If your preview can change or rotate, take care of those events here.
-        // Make sure to stop the preview before resizing or reformatting it.
-
         if (mHolder.getSurface() == null) {
-            // preview surface does not exist
             return;
         }
 
-        // stop preview before making changes
         try {
             mCamera.stopPreview();
         } catch (Exception e) {
             // ignore: tried to stop a non-existent preview
         }
 
-        // set preview size and make any resize, rotate or
-        // reformatting changes here
         if (null != mPreviewSize) {
             Camera.Parameters parameters = mCamera.getParameters();
             parameters.setPreviewSize(mPreviewSize.width, mPreviewSize.height);
@@ -72,12 +60,9 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
             mCamera.startPreview();
         }
 
-        // start preview with new settings
         try {
-
             mCamera.setPreviewDisplay(mHolder);
             mCamera.startPreview();
-
         } catch (Exception e) {
             Log.d(TAG, "Error starting camera preview: " + e.getMessage());
         }
--- Unify.Android/app/src/main/java/com/owlcreativestudio/unify/Helpers/UnifyCommonHelpers.java
+++ Unify.Android/app/src/main/java/com/owlcreativestudio/unify/Helpers/UnifyCommonHelpers.java
@@ -1,24 +1,5 @@
 package com.owlcreativestudio.unify.Helpers;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.os.Build;
-import android.view.View;
-
-/**
- * Created by aipir on 31-Jan-17.
- */
-
 public class UnifyCommonHelpers {
-    public static void simulateWait() {
-        simulateWait(2000);
-    }
 
-    public static void simulateWait(int miliseconds) {
-        try {
-            Thread.sleep(miliseconds);
-        } catch (InterruptedException e) {
-        }
-    }
 }
